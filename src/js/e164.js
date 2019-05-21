7aaaa749d0f3e4430dc0efba0d853cc774e9227d
Ryan Spain <rspain+github@outlook.ie>
2019-05-22 00:03:59 +0100

    take this github

---
+++ src/js/e164.js
@@ -0,0 +1,35 @@
+function formatNumber(phoneNumber, input) {
+
+    let formattedNumber = phoneNumber
+
+        // Remove all characters but digits and +
+        .replace(/\D|\+/g, '')
+
+        // Remove international dialing prefix
+        .replace(/^[+00]{3}|^[00]{2}/g, '+')
+
+        // Add Irish country prefix
+        .replace(/^0(?=[1-9])/, '+353');
+
+    // Validate result
+    alert(validatedNumber(formattedNumber));
+
+    // If an input element was provided - update its value
+    if (phoneNumber)
+        input.value = formattedNumber
+    // Else - return the formatted number
+    else
+        return formattedNumber;
+}
+
+function validatedNumber(phoneNumber) {
+
+    // Gaurd against number lenght
+    if (phoneNumber.length > 15 || phoneNumber < 8) return false;
+
+    // Allow potentially correct phone numbers
+    if (phoneNumber.startsWith('+')) return true;
+
+    // Validated formatted phone number structure
+    if(/^\+?[1-9]\d{1,14}$/g.test(phoneNumber)) true;
+}
